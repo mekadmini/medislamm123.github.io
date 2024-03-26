ee3188dcf7edb24691d2905690e6e0dec6d62ee9
mekadmini <m.mekadmini@campus.lmu.de>
2024-03-26 15:22:51 +0100

    update navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,14 +3,16 @@ import styles from "./Navbar.module.css"
 import { getImageUrl } from "../../utils"
 
 export const Navbar = () => {
-
+    const [menuOpen, setMenuOpen] = useState(false);
 
     return (
         <>
             <nav className={styles.navbar}>
                 <a className={styles.title} href="/">Portfolio</a>
                 <div className={styles.menu}>
-                    <ul className={styles.menuItems}>
+                    <img className={styles.MenuBtn} src={menuOpen ? getImageUrl("nav/menuIcon.png") : getImageUrl("nav/menuIcon.png")} alt="menu-dash-button" width="40" onClick={() => setMenuOpen(!menuOpen)}></img>
+                    {/* if the menu is open apply the corresponding style */}
+                    <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}>
                         <li><a href="#about">About</a></li>
                         <li><a href="#experience">Experience</a></li>
                         <li><a href="#projects">Projects</a></li>
